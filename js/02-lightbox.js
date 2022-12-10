be63887d9d304ddbe1dcb2c493195bd75ff5939d
Andrey Obibok <andreyobibok@gmail.com>
2022-12-10 17:11:14 +0200

    HW ready

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const markup = galleryItems.map((element) =>
+    `<a class="gallery__item" href=${element.original}>
+        <img class="gallery__image" src=${element.preview} alt=${element.description} />
+    </a>`
+).join("");
+
+gallery.insertAdjacentHTML('beforeend', markup);
+
+gallery.addEventListener('click', handleClick);
+function handleClick(event) {
+    event.preventDefault();
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: '250ms',
+});
